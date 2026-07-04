48b2080ce2a04b96d9537efba0814ba437be9110
Kyon Li <i@kyonli.com>
2026-07-04 18:46:20 +0800

    Update kernel to 6.12.94

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
 DIST=trixie
-BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot.img"
-BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot-no-modem.img"
-BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/boot-no-modem-oc.img"
-K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.49-1/linux-image-6.12.49-msm8916-g93a71ee9468d_6.12.49-g93a71ee9468d-1_arm64.deb"
-K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.12.49-1"
+BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.94-1/boot.img"
+BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.94-1/boot-no-modem.img"
+BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.94-1/boot-no-modem-oc.img"
+K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.12.94-1/linux-image-6.12.94-msm8916-gaeb7498348fd_6.12.94-gaeb7498348fd-1_arm64.deb"
+K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.12.94-1"
 UUID=62ae670d-01b7-4c7d-8e72-60bcd00410b7
 
 if [ `id -u` -ne 0 ]
